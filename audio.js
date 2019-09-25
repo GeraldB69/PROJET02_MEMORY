c2d9b793fd2758338e97958b9d6e74d430faad8a
Anicia Tarlao <tarlaoanicia@gmail.com>
2019-09-25 15:27:07 +0200

    Modif ID audio

--- audio.js
+++ audio.js
@@ -1,77 +1,89 @@
-// const paires1 = document.getElementById("id14");
-// const audio1 = document.getElementById("audio1");
-
-// paires1.addEventListener("click", function (){
-//     audio1.play();
-// })
-
-// const paires2 = document.getElementById("paires2");
-// const audio2 = document.getElementById("audio2");
-
-// paires2.addEventListener("click", function (){
-//     audio2.play();
-// })
-
-const paires1 = document.getElementsByClassName("son1");
 const audio1 = document.getElementById("audio1");
-for (let i=0; i < paires1.length; i++) {
-    paires1[i].addEventListener("click", function(){
-    audio1.play();
-    });
-}
-
-const paires2 = document.getElementsByClassName("son2");
 const audio2 = document.getElementById("audio2");
-for (let i=0; i < paires2.length; i++) {
-    paires2[i].addEventListener("click", function(){
-    audio2.play();
-    });
-}
+const audio3 = document.getElementById("audio3");
+const audio4 = document.getElementById("audio4");
+const audio5 = document.getElementById("audio5");
+const audio6 = document.getElementById("audio6");
+const audio7 = document.getElementById("audio7");
+const audio8 = document.getElementById("audio8");
 
-const paires3 = document.getElementsByClassName("son3");
-const audio3 = document.getElementById("audio1");
-for (let i=0; i < paires3.length; i++) {
-    paires3[i].addEventListener("click", function(){
+
+const img1 = document.getElementById("id14");
+img1.addEventListener("click", function (){
+    audio1.play();
+})
+
+const img2 = document.getElementById("id15");
+img2.addEventListener("click", function (){
+    audio2.play();
+})
+
+const img3 = document.getElementById("id18");
+img3.addEventListener("click", function (){
     audio3.play();
-    });
-}
+})
 
-const paires4 = document.getElementsByClassName("son4");
-const audio4 = document.getElementById("audio2");
-for (let i=0; i < paires4.length; i++) {
-    paires4[i].addEventListener("click", function(){
-    audio2.play();
-    });
-}
+const img4 = document.getElementById("id20");
+img4.addEventListener("click", function (){
+    audio4.play();
+})
 
-const paires5 = document.getElementsByClassName("son5");
-const audio5 = document.getElementById("audio1");
-for (let i=0; i < paires5.length; i++) {
-    paires5[i].addEventListener("click", function(){
+const img5 = document.getElementById("id22");
+img5.addEventListener("click", function (){
+    audio5.play();
+})
+
+const img6 = document.getElementById("id25");
+img6.addEventListener("click", function (){
+    audio6.play();
+})
+
+const img7 = document.getElementById("id26");
+img7.addEventListener("click", function (){
+    audio7.play();
+})
+
+const img8 = document.getElementById("id28");
+img8.addEventListener("click", function (){
+    audio8.play();
+})
+
+const img9 = document.getElementById("id31");
+img9.addEventListener("click", function (){
     audio1.play();
-    });
-}
+})
 
-const paires6 = document.getElementsByClassName("son6");
-const audio6 = document.getElementById("audio2");
-for (let i=0; i < paires6.length; i++) {
-    paires6[i].addEventListener("click", function(){
+const img10 = document.getElementById("id34");
+img10.addEventListener("click", function (){
     audio2.play();
-    });
-}
+})
 
-const paires7 = document.getElementsByClassName("son7");
-const audio7 = document.getElementById("audio1");
-for (let i=0; i < paires7.length; i++) {
-    paires7[i].addEventListener("click", function(){
-    audio1.play();
-    });
-}
+const img11 = document.getElementById("id35");
+img11.addEventListener("click", function (){
+    audio3.play();
+})
 
-const paires8 = document.getElementsByClassName("son8");
-const audio8 = document.getElementById("audio2");
-for (let i=0; i < paires8.length; i++) {
-    paires8[i].addEventListener("click", function(){
-    audio2.play();
-    });
-}
+const img12 = document.getElementById("id39");
+img12.addEventListener("click", function (){
+    audio4.play();
+})
+
+const img13 = document.getElementById("id43");
+img13.addEventListener("click", function (){
+    audio5.play();
+})
+
+const img14 = document.getElementById("id46");
+img14.addEventListener("click", function (){
+    audio6.play();
+})
+
+const img15 = document.getElementById("id48");
+img15.addEventListener("click", function (){
+    audio7.play();
+})
+
+const img16 = document.getElementById("id53");
+img16.addEventListener("click", function (){
+    audio8.play();
+})
