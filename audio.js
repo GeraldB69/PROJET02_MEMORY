e0ba154d91ad524b6e20b4f26b5a74b5baefde32
Oliv <oallardjacquin@gmail.com>
2019-09-26 16:19:37 +0200

    Fonction lire et retourner id audio ok

--- audio.js
+++ audio.js
@@ -44,93 +44,21 @@ const idBonhommes = [
 	"id53"
 ];
 
+let soundPlayed1 = 0;
+let soundPlayed2 = 0;
+
 for (let i = 0; i < idBonhommes.length; i++) {
 	const img = document.getElementById(idBonhommes[i]);
 	img.addEventListener("click", function() {
 		audios[i].play();
 		console.log(audios[i].id);
-		console.log(idBonhommes[i]);
+		//console.log(idBonhommes[i]);
+		if (soundPlayed1 === 0) {
+			soundPlayed1 = audios[i].id;
+			console.log(soundPlayed1);
+		} else if (soundPlayed2 === 0) {
+			soundPlayed2 = audios[i].id;
+			console.log(soundPlayed2);
+		}
 	});
 }
-
-/* const img1 = document.getElementById("id14");
-img1.addEventListener("click", function() {
-	audio1.play();
-	console.log(audio1.id);
-});
-
-const img2 = document.getElementById("id15");
-img2.addEventListener("click", function() {
-	audio2.play();
-});
-
-const img3 = document.getElementById("id18");
-img3.addEventListener("click", function() {
-	audio3.play();
-});
-
-const img4 = document.getElementById("id20");
-img4.addEventListener("click", function() {
-	audio4.play();
-});
-
-const img5 = document.getElementById("id22");
-img5.addEventListener("click", function() {
-	audio5.play();
-});
-
-const img6 = document.getElementById("id25");
-img6.addEventListener("click", function() {
-	audio6.play();
-});
-
-const img7 = document.getElementById("id26");
-img7.addEventListener("click", function() {
-	audio7.play();
-});
-
-const img8 = document.getElementById("id28");
-img8.addEventListener("click", function() {
-	audio8.play();
-});
-
-const img9 = document.getElementById("id31");
-img9.addEventListener("click", function() {
-	audio1.play();
-});
-
-const img10 = document.getElementById("id34");
-img10.addEventListener("click", function() {
-	audio2.play();
-});
-
-const img11 = document.getElementById("id35");
-img11.addEventListener("click", function() {
-	audio3.play();
-});
-
-const img12 = document.getElementById("id39");
-img12.addEventListener("click", function() {
-	audio4.play();
-});
-
-const img13 = document.getElementById("id43");
-img13.addEventListener("click", function() {
-	audio5.play();
-});
-
-const img14 = document.getElementById("id46");
-img14.addEventListener("click", function() {
-	audio6.play();
-});
-
-const img15 = document.getElementById("id48");
-img15.addEventListener("click", function() {
-	audio7.play();
-});
-
-const img16 = document.getElementById("id53");
-img16.addEventListener("click", function() {
-	audio8.play();
-});
- */
