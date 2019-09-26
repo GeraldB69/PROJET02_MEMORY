ffa5caf379157e6a72398a51d42f07879c6445b1
Oliv <oallardjacquin@gmail.com>
2019-09-26 08:33:13 +0200

    save

--- audio.js
+++ audio.js
@@ -7,83 +7,83 @@ const audio6 = document.getElementById("audio6");
 const audio7 = document.getElementById("audio7");
 const audio8 = document.getElementById("audio8");
 
-
 const img1 = document.getElementById("id14");
-img1.addEventListener("click", function (){
-    audio1.play();
-})
+img1.addEventListener("click", function() {
+	audio1.play();
+	console.log(audio1.getElementById(id));
+});
 
 const img2 = document.getElementById("id15");
-img2.addEventListener("click", function (){
-    audio2.play();
-})
+img2.addEventListener("click", function() {
+	audio2.play();
+});
 
 const img3 = document.getElementById("id18");
-img3.addEventListener("click", function (){
-    audio3.play();
-})
+img3.addEventListener("click", function() {
+	audio3.play();
+});
 
 const img4 = document.getElementById("id20");
-img4.addEventListener("click", function (){
-    audio4.play();
-})
+img4.addEventListener("click", function() {
+	audio4.play();
+});
 
 const img5 = document.getElementById("id22");
-img5.addEventListener("click", function (){
-    audio5.play();
-})
+img5.addEventListener("click", function() {
+	audio5.play();
+});
 
 const img6 = document.getElementById("id25");
-img6.addEventListener("click", function (){
-    audio6.play();
-})
+img6.addEventListener("click", function() {
+	audio6.play();
+});
 
 const img7 = document.getElementById("id26");
-img7.addEventListener("click", function (){
-    audio7.play();
-})
+img7.addEventListener("click", function() {
+	audio7.play();
+});
 
 const img8 = document.getElementById("id28");
-img8.addEventListener("click", function (){
-    audio8.play();
-})
+img8.addEventListener("click", function() {
+	audio8.play();
+});
 
 const img9 = document.getElementById("id31");
-img9.addEventListener("click", function (){
-    audio1.play();
-})
+img9.addEventListener("click", function() {
+	audio1.play();
+});
 
 const img10 = document.getElementById("id34");
-img10.addEventListener("click", function (){
-    audio2.play();
-})
+img10.addEventListener("click", function() {
+	audio2.play();
+});
 
 const img11 = document.getElementById("id35");
-img11.addEventListener("click", function (){
-    audio3.play();
-})
+img11.addEventListener("click", function() {
+	audio3.play();
+});
 
 const img12 = document.getElementById("id39");
-img12.addEventListener("click", function (){
-    audio4.play();
-})
+img12.addEventListener("click", function() {
+	audio4.play();
+});
 
 const img13 = document.getElementById("id43");
-img13.addEventListener("click", function (){
-    audio5.play();
-})
+img13.addEventListener("click", function() {
+	audio5.play();
+});
 
 const img14 = document.getElementById("id46");
-img14.addEventListener("click", function (){
-    audio6.play();
-})
+img14.addEventListener("click", function() {
+	audio6.play();
+});
 
 const img15 = document.getElementById("id48");
-img15.addEventListener("click", function (){
-    audio7.play();
-})
+img15.addEventListener("click", function() {
+	audio7.play();
+});
 
 const img16 = document.getElementById("id53");
-img16.addEventListener("click", function (){
-    audio8.play();
-})
+img16.addEventListener("click", function() {
+	audio8.play();
+});
